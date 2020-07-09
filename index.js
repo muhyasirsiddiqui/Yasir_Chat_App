afe9b93c11e81e388ed5b7e94be4b69439c1da3b
Muhammad Yasir <Muhammad.Yasir@avanza.pk>
2020-07-09 19:53:57 +0500

    some changes

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, u
   // app.get('*', (req,res) => { 
   //   res.sendFile(path.join(__dirname, '..', 'frontend', 'build','index.html'))
   // });
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, '../public')));
   app.get('*', (req,res) => { 
-    res.sendFile(path.join(__dirname,'../build','index.html'))
+    res.sendFile(path.join(__dirname,'../public','index.html'))
   });
 }  
 
