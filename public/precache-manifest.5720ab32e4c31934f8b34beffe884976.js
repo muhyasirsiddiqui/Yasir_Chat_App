d81fae02bbfbe159da81c6d80b0d6f521b9858f2
Muhammad Yasir <Muhammad.Yasir@avanza.pk>
2020-07-09 19:10:06 +0500

    First commit

---
+++ public/precache-manifest.5720ab32e4c31934f8b34beffe884976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99ff76eff1b77f1656faa9bb66936b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cde665998ea7e10b913",
+    "url": "/static/css/main.69b9ba7d.chunk.css"
+  },
+  {
+    "revision": "e7c705379119d0e168ba",
+    "url": "/static/js/2.b9d9a311.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.b9d9a311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cde665998ea7e10b913",
+    "url": "/static/js/main.00aeb411.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
